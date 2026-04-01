5d01b5062f70004acd1eafb0b2516a7e51918331
NSagan271 <nsagan@stanford.edu>
2026-04-01 23:47:53 +0000

    modify script

--- perf_testing/offline_homogenous.sh
+++ perf_testing/offline_homogenous.sh
@@ -2,28 +2,41 @@
 
 #!/bin/bash
 
-NUM_REQUESTS="${NUM_REQUESTS:-128}"
+#!/bin/bash
 
 BATCH_SIZES=(1 2 4 8 16)
+NUM_BATCHES="${NUM_BATCHES:-10}"
+MIN_NUM_REQUESTS="${MIN_NUM_REQUESTS:-20}"
+
+compute_num_requests() {
+  local bs=$1
+  local req=$((NUM_BATCHES * bs))
+  if [ "$req" -lt "$MIN_NUM_REQUESTS" ]; then
+    req=$MIN_NUM_REQUESTS
+  fi
+  echo $req
+}
 
 # text_to_text (with DATASET=text)
 for bs in "${BATCH_SIZES[@]}"; do
+  NUM_REQUESTS=$(compute_num_requests $bs)
   NUM_REQUESTS=$NUM_REQUESTS TASK=text_to_text DATASET=text BATCH_SIZE=$bs benchmark/run_benchmark.sh
 done
 
 # image_to_text
 for bs in "${BATCH_SIZES[@]}"; do
+  NUM_REQUESTS=$(compute_num_requests $bs)
   NUM_REQUESTS=$NUM_REQUESTS TASK=image_to_text BATCH_SIZE=$bs benchmark/run_benchmark.sh
 done
 
-NUM_REQUESTS="${NUM_REQUESTS_GEN:-64}"
-
 # text_to_image
 for bs in "${BATCH_SIZES[@]}"; do
+  NUM_REQUESTS=$(compute_num_requests $bs)
   NUM_REQUESTS=$NUM_REQUESTS TASK=text_to_image BATCH_SIZE=$bs benchmark/run_benchmark.sh
 done
 
 # image_to_image
 for bs in "${BATCH_SIZES[@]}"; do
+  NUM_REQUESTS=$(compute_num_requests $bs)
   NUM_REQUESTS=$NUM_REQUESTS TASK=image_to_image BATCH_SIZE=$bs benchmark/run_benchmark.sh
 done
